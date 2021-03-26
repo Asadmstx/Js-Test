361caa1999debb847282a923ff48a5d8bd0ee6eb
Asadmstx <asadmstx19@gmail.com>
2021-03-26 23:42:23 +0500

    js test

---
+++ test.js
@@ -0,0 +1,38 @@
+var myName = prompt("Ismingizni kiriting")
+
+alert ('Salom ;) ' + myName)
+
+alert (myName + '! ' + 'davom etish uchun ok tugmasini bosing va misollarni yeching')
+
+var answer01 = prompt('[01/10] ' + ' 10 + 10 =') /* 20 */
+
+var answer02 = prompt('[02/10] ' + ' 5 + 5 - 5 =') /* 5 */
+
+var answer03 = prompt('[03/10] ' + ' 2 + 2 * 2 =') /* 6 */
+
+var answer04 = prompt('[04/10] ' + ' 12 : 4 + 3 =') /* 6 */
+
+var answer05 = prompt('[05/10] ' + ' 5 * 5 - 5 =') /* 20 */
+
+var answer06 = prompt('[06/10] ' + ' 15 - 5 * 3 =') /* 0 */
+
+var answer07 = prompt('[07/10] ' + ' (4 - 4) * 7 =') /* 0 */
+
+var answer08 = prompt('[08/10] ' + ' 10 + 5 - 12 =') /* 3 */
+
+var answer09 = prompt('[09/10] ' + ' 10 * 10 + 100 =') /* 200 */
+
+var answer10 = prompt('[10/10] ' + ' (45 + 55) * 5 =') /* 500 */
+
+alert ('Bu oxirgi misol edi. Javoblarni lorish uchun kansolni oching')
+
+console.log('[01/10] ' + ' 10 + 10 = ' + answer01 + ' (tog`ri javob 20)');
+console.log('[02/10] ' + ' 5 + 5 - 5 = ' + answer02 + ' (tog`ri javob 5)');
+console.log('[03/10] ' + ' 2 + 2 * 2 = ' + answer03 + ' (tog`ri javob 6)');
+console.log('[04/10] ' + ' 12 : 4 + 3 = ' + answer04 + ' (tog`ri javob 6)');
+console.log('[05/10] ' + ' 5 * 5 - 5 = ' + answer05 + ' (tog`ri javob 20)');
+console.log('[06/10] ' + ' 15 - 5 * 3 = ' + answer06 + ' (tog`ri javob 0)');
+console.log('[07/10] ' + ' (4 - 4) * 7 = ' + answer07 + ' (tog`ri javob 0)');
+console.log('[08/10] ' + ' 10 + 5 - 12 = ' + answer08 + ' (tog`ri javob 3)');
+console.log('[09/10] ' + ' 10 * 10 + 100 = ' + answer09 + ' (tog`ri javob 200)');
+console.log('[10/10] ' + ' (45 + 55) * 5 = ' + answer10 + ' (tog`ri javob 500)');
